ce10892ac0d170f2a9ac7a2020531d0ca18b9e3e
Mizanur Rahman <mizzanur007@gmail.com>
2021-03-24 11:20:32 +0600

    45-5 dynamic api, api parameter, access params, access query

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 
 const app = express();
-app.get("/", (req, res) => res.send("Thank you so much"));
+app.get("/", (req, res) => {
+  const fruit = {
+    product: "banana",
+    price: 200,
+  };
+  res.send(fruit);
+});
+
+const users = ["Asad", "Fasad", "Sajad", "Nazad"];
+
+app.get("/fruits/banana", (req, res) => {
+  res.send({ fruit: "banana", quantity: 1000, price: 20000 });
+});
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const name = users[id];
+  res.send({ id, name });
+});
 
 app.listen(4000, () => console.log("Listening to port 4000"));
